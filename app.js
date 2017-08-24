848d93f7fdb502f1bbf7a1f52bf04fb91a681bfb
Sebastian Chikan <cseby92@gmail.com>
2017-08-24 20:40:54 +0200

    tests refactored, function moved into a class, with a factory helperclass

--- app.js
+++ app.js
@@ -6,97 +6,147 @@ const WORK_HOURS = WORK_END_HOURS - WORK_START_HOURS;
 const SATURDAY = 6;
 const SUNDAY = 0;
 
-function ordCalculateDueDate(submitDate, turnAroundTime) {
+class WorkScheduleFactory{
 
-    if (turnAroundTime < 0)
-        throw new Error('Turnaround time must be >= 0');
-    if (isWeekend(submitDate))
-        throw new Error('Submit date is a weekend day');
-    if (turnAroundTime === 0)
-        return submitDate;
-    if (isStillThisWorkday(submitDate, turnAroundTime)) {
-        return addHours(submitDate, turnAroundTime);
-    } else {
-        return getOverLappingDate(submitDate, turnAroundTime);
+    createSchedule(workStartHours, workEndHours, saturday, sunday){
+        return {
+            //todo saturday sunday lekezelése, hogy ne számot kelljen beírni
+            //todo később munkaszüneti napok 1 tömbbe dinamikusan
+            workStartHours : workStartHours,
+            workEndHours : workEndHours,
+            saturday: saturday,
+            sunday : sunday
+        }
+    }
+
+    createBasicSchedule(){
+        return {
+            workStartHours : 9,
+            workEndHours : 17,
+            saturday: 6,
+            sunday : 0
+        }
     }
 }
 
-function isStillThisWorkday(date, hoursToAdd) {
-    let onTime = addHours(date, hoursToAdd).getHours() <= WORK_END_HOURS;
-    let today = addHours(date, hoursToAdd).getDate() === date.getDate();
+class DueToDateCalculator{
 
-    return onTime && today;
-}
-
-function addHours(date, hours) {
-    return new Date(date.getTime() + hours * 60000 * 60);
-}
-
-function getOverLappingDate(submitDate, turnAroundTime) {
-    let daysToAdd = calculateDaysToAdd(submitDate, turnAroundTime);
-    let hoursToAdd = calculateHoursToAdd(submitDate, turnAroundTime);
-
-    return addHoursAndDaysToDate(submitDate, daysToAdd, hoursToAdd);
-}
-
-function calculateDaysToAdd(date, hours) {
-    let days = Math.floor((date.getHours() - WORK_START_HOURS + hours) / WORK_HOURS);
-    let weekendDays = countWeekEndDays(date, hours);
-
-    return days + weekendDays;
-}
-
-function calculateHoursToAdd(date, hours) {
-    return (date.getHours() - WORK_START_HOURS + hours) % WORK_HOURS;
-}
-
-function addHoursAndDaysToDate(date, days, hours){
-    let daysAdded = addDays(date, days);
-    return daysAdded.setHours(WORK_START_HOURS + hours);
-}
-
-function countWeekEndDays(date, hours) {
-    let tempDate = new Date(date);
-    let hoursToDays = Math.floor(hours / 8);
-    let remainingHours = hours % 8;
-    let weekEndDays = calculateWeekeendDaysByDays(tempDate, hoursToDays);
-    weekEndDays += calculateWeekendByRemainingHours(tempDate, remainingHours);
-
-    return weekEndDays;
-}
-
-function calculateWeekeendDaysByDays(date, days) {
-    let weekEndDays = 0;
-    let tempDate = new Date(date);
-    while (days !== 0) {
-        tempDate = addDays(tempDate, 1);
-        if (isWeekend(tempDate)) {
-            weekEndDays += 2;
-            tempDate = addDays(tempDate, 1);
-        } else
-            days--;
+    constructor(workSchedule){
+        if(!workSchedule){
+            this.WORK_START_HOURS = 9;
+            this.WORK_END_HOURS = 17;
+            this.WORK_HOURS = WORK_END_HOURS - WORK_START_HOURS;
+            this.SATURDAY = 6;
+            this.SUNDAY = 0;
+        }else{
+            this.WORK_START_HOURS = workSchedule.workStartHours;
+            this.WORK_END_HOURS = workSchedule.workEndHours;
+            this.WORK_HOURS = this.WORK_END_HOURS - this.WORK_START_HOURS;
+            this.SATURDAY = workSchedule.saturday;
+            this.SUNDAY = workSchedule.sunday;
+        }
     }
-    return weekEndDays;
-}
 
-function calculateWeekendByRemainingHours(date, hours) {
-    let weekEndDays = 0;
-    date = addHours(date, hours);
-    if (date.getHours() > 17) {
-        date = addDays(date, 1);
-        if (isWeekend(date))
-            weekEndDays += 2;
+    ordCalculateDueDate(submitDate, turnAroundTime) {
+
+        let a = new DueToDateCalculator();
+
+        if (turnAroundTime < 0)
+            throw new Error('Turnaround time must be >= 0');
+        if (this.isWeekend(submitDate))
+            throw new Error('Submit date is a weekend day');
+        if (turnAroundTime === 0)
+            return submitDate;
+        if (this.isStillThisWorkday(submitDate, turnAroundTime)) {
+            return this.addHours(submitDate, turnAroundTime);
+        } else {
+            return new Date(this.getOverLappingDate(submitDate, turnAroundTime));
+        }
     }
-    return weekEndDays;
+
+    //todo: hide theese private functions????
+
+    isStillThisWorkday(date, hoursToAdd) {
+
+        let onTime = this.addHours(date, hoursToAdd).getHours() <= WORK_END_HOURS;
+        let today = this.addHours(date, hoursToAdd).getDate() === date.getDate();
+
+        return onTime && today;
+    }
+
+    addHours(date, hours) {
+        return new Date(date.getTime() + hours * 60000 * 60);
+    }
+
+    getOverLappingDate(submitDate, turnAroundTime) {
+        let daysToAdd = this.calculateDaysToAdd(submitDate, turnAroundTime);
+        let hoursToAdd = this.calculateHoursToAdd(submitDate, turnAroundTime);
+
+        return this.addHoursAndDaysToDate(submitDate, daysToAdd, hoursToAdd);
+    }
+
+    calculateDaysToAdd(date, hours) {
+        let days = Math.floor((date.getHours() - WORK_START_HOURS + hours) / WORK_HOURS);
+        let weekendDays = this.countWeekEndDays(date, hours);
+
+        return days + weekendDays;
+    }
+
+    calculateHoursToAdd(date, hours) {
+        return (date.getHours() - WORK_START_HOURS + hours) % WORK_HOURS;
+    }
+
+    addHoursAndDaysToDate(date, days, hours){
+        let daysAdded = this.addDays(date, days);
+        return daysAdded.setHours(WORK_START_HOURS + hours);
+    }
+
+    countWeekEndDays(date, hours) {
+        let tempDate = new Date(date);
+        let hoursToDays = Math.floor(hours / 8);
+        let remainingHours = hours % 8;
+        let weekEndDays = this.calculateWeekeendDaysByDays(tempDate, hoursToDays);
+        weekEndDays += this.calculateWeekendByRemainingHours(tempDate, remainingHours);
+
+        return weekEndDays;
+    }
+
+    calculateWeekeendDaysByDays(date, days) {
+        let weekEndDays = 0;
+        let tempDate = new Date(date);
+        while (days !== 0) {
+            tempDate = this.addDays(tempDate, 1);
+            if (this.isWeekend(tempDate)) {
+                weekEndDays += 2;
+                tempDate = this.addDays(tempDate, 1);
+            } else
+                days--;
+        }
+        return weekEndDays;
+    }
+
+    calculateWeekendByRemainingHours(date, hours) {
+        let weekEndDays = 0;
+        date = this.addHours(date, hours);
+        if (date.getHours() > 17) {
+            date = this.addDays(date, 1);
+            if (this.isWeekend(date))
+                weekEndDays += 2;
+        }
+        return weekEndDays;
+    }
+
+    isWeekend(date) {
+        let day = date.getDay();
+        return day === SUNDAY || day === SATURDAY;
+    }
+
+    addDays(date, days) {
+        let tempDate = new Date(date);
+        return new Date(tempDate.setDate(tempDate.getDate() + days));
+    }
 }
 
-function isWeekend(date) {
-    let day = date.getDay();
-    return day === SUNDAY || day === SATURDAY;
-}
+let a = new DueToDateCalculator(WorkScheduleFactory.createBasicSchedule);
 
-function addDays(date, days) {
-    let tempDate = new Date(date);
-    return new Date(tempDate.setDate(tempDate.getDate() + days));
-}
-module.exports = ordCalculateDueDate;
+module.exports = {DueToDateCalculator, WorkScheduleFactory};
--- tests.js
+++ tests.js
@@ -1,83 +1,105 @@
 'use strict';
 
 const expect = require('chai').expect;
-const ordCalculateDueDate = require('./app');
-
+const workScheduleMethods = require('./app');
+const WorkScheduleFactory = workScheduleMethods.WorkScheduleFactory;
+const DueToDateCalculator = workScheduleMethods.DueToDateCalculator;
 describe('CalculateDueDate ', function() {
 
-    it('should return the same date for 0 hours', () => {
-        let now = new Date();
-        expect(ordCalculateDueDate(now, 0)).to.eql(now);
+    let testData = [
+        {
+            testText : 'should return the same date for 0 hours',
+            startDate : new Date(),
+            hoursToAdd : 0,
+            toEql : new Date()
+        },
+        {
+            testText : 'should return the plus 2 days plus the overlapping working hours',
+            startDate : new Date(2017, 7, 2, 16, 30),
+            hoursToAdd : 11,
+            toEql : new Date(2017, 7, 4, 11, 30)
+        },
+        {
+            testText : 'should return the next day plus the overlapping working hours',
+            startDate : new Date(2017, 7, 3, 16, 30),
+            hoursToAdd : 3,
+            toEql : new Date(2017, 7, 4, 11, 30)
+        },
+        {
+            testText : 'should return the plus 2 days plus the overlapping working hours',
+            startDate : new Date(2017, 7, 2, 16, 30),
+            hoursToAdd : 11,
+            toEql : new Date(2017, 7, 4, 11, 30)
+        },        {
+            testText : 'should return plus 1 day and jump to the next month',
+            startDate : new Date(2017, 7, 31, 16, 0),
+            hoursToAdd : 8,
+            toEql : new Date(2017, 8, 1, 16, 0)
+        },
+        {
+            testText : 'should return the the next workday skip the weekend plus the overlapping working hours',
+            startDate : new Date(2017, 7, 4, 16, 30),
+            hoursToAdd : 3,
+            toEql : new Date(2017, 7, 7, 11, 30)
+        },
+        {
+            testText : 'should return the date plus 3 days and skip the weekend plus the overlapping working hours',
+            startDate : new Date(2017, 7, 4, 16, 30),
+            hoursToAdd : 3 * 8,
+            toEql : new Date(2017, 7, 9, 16, 30)
+        },
+        {
+            testText : 'should return the date plus 6 days and skip the weekend',
+            startDate : new Date(2017, 7, 2, 16, 30),
+            hoursToAdd : 6 * 8,
+            toEql : new Date(2017, 7, 10, 16, 30)
+        },
+        {
+            testText : 'should return the date plus 16 days and skip the weekends',
+            startDate : new Date(2017, 7, 2, 16, 30),
+            hoursToAdd : 16 * 8,
+            toEql : new Date(2017, 7, 24, 16, 30)
+        },
+        {
+            testText : 'should return the date plus 46 days and skip the weekends',
+            startDate : new Date(2017, 7, 2, 16, 30),
+            hoursToAdd : 46 * 8,
+            toEql : new Date(2017, 9, 5, 16, 30)
+        },
+        {
+            testText : 'should return the date plus 46 days plus 3 hours and skip the weekends',
+            startDate : new Date(2017, 7, 2, 16, 30),
+            hoursToAdd : (46 * 8) + 3,
+            toEql : new Date(2017, 9, 6, 11, 30)
+        }
+    ];
+
+    let factory = new WorkScheduleFactory();
+    let calculator = new DueToDateCalculator(factory.createBasicSchedule());
+
+    testData.forEach((data) => {
+        it(data.testText, () => {
+            expect(calculator.ordCalculateDueDate(data.startDate, data.hoursToAdd))
+                .to.eql(data.toEql);
+        });
     });
-
-    it('should return the same date plus 1 hour for 1 hours (no overlap)', () => {
-        let now = new Date(2017, 7, 3, 9, 30);
-        expect(ordCalculateDueDate(now, 1)).to.eql(addMinutes(now, 60));
-    });
-
-    it('should return the next day plus the overlapping working hours', () => {
-        let now = new Date(2017, 7, 3, 16, 30);
-        expect(ordCalculateDueDate(now, 3)).to.eql(new Date(2017, 7, 4, 11, 30));
-    });
-
-    it('should return the plus 2 days plus the overlapping working hours', () => {
-        let now = new Date(2017, 7, 2, 16, 30);
-        expect(ordCalculateDueDate(now, 11)).to.eql(new Date(2017, 7, 4, 11, 30));
-    });
-
-    it('should return plus 1 day and jump to the next month', () => {
-        let now = new Date(2017, 7, 31, 16, 0);
-        expect(ordCalculateDueDate(now, 8)).to.eql(new Date(2017, 8, 1, 16, 0));
-    });
-
-    it('should return plus 1 day and jump to the next year and month', () => {
-        let now = new Date(2015, 11, 31, 16, 0);
-        expect(ordCalculateDueDate(now, 8)).to.eql(new Date(2016, 0, 1, 16, 0));
-    });
-
-    it('should return the the next workday skip the weekend plus the overlapping working hours', () => {
-        let now = new Date(2017, 7, 4, 16, 30);
-        expect(ordCalculateDueDate(now, 3)).to.eql(new Date(2017, 7, 7, 11, 30));
-    });
-
-    it('should return the date plus 3 days and skip the weekend plus the overlapping working hours', () => {
-        let now = new Date(2017, 7, 4, 16, 30);
-        expect(ordCalculateDueDate(now, 3 * 8)).to.eql(new Date(2017, 7, 9, 16, 30));
-    });
-
-    it('should return the date plus 6 days and skip the weekend', () => {
-        let now = new Date(2017, 7, 2, 16, 30);
-        expect(ordCalculateDueDate(now, 6 * 8)).to.eql(new Date(2017, 7, 10, 16, 30));
-    });
-
-    it('should return the date plus 16 days and skip the weekends', () => {
-        let now = new Date(2017, 7, 2, 16, 30);
-        expect(ordCalculateDueDate(now, 16 * 8)).to.eql(new Date(2017, 7, 24, 16, 30));
-    });
-
-
-    it('should return the date plus 46 days and skip the weekends', () => {
-        let now = new Date(2017, 7, 2, 16, 30);
-        expect(ordCalculateDueDate(now, 46 * 8)).to.eql(new Date(2017, 9, 5, 16, 30));
-    });
-
 
     it('should return the date plus 46 days plus 3 hours and skip the weekends', () => {
         let now = new Date(2017, 7, 2, 16, 30);
-        expect(ordCalculateDueDate(now, (46 * 8) + 3)).to.eql(new Date(2017, 9, 6, 11, 30));
+        expect(calculator.ordCalculateDueDate(now, (46 * 8) + 3)).to.eql(new Date(2017, 9, 6, 11, 30));
     });
 
     it('should return an Error: "Submit date is a weekend day"', () => {
         let now = new Date(2017, 7, 5, 16, 30);
         expect(function() {
-            ordCalculateDueDate(now, 123213);
+            calculator.ordCalculateDueDate(now, 123213);
         }).to.throw(Error, 'Submit date is a weekend day');
     });
 
     it('should return an Error: "Turnaround time must be >= 0"', () => {
         let now = new Date(2017, 7, 5, 16, 30);
         expect(function() {
-            ordCalculateDueDate(now, -6);
+            calculator.ordCalculateDueDate(now, -6);
         }).to.throw(Error, 'Turnaround time must be >= 0');
     });
 });
